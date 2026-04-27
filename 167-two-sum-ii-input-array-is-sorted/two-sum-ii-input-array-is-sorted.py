c2aa24e61e9394c542718ac73cb4ba370959f533
JacksonTran <107548088+JacksonHana@users.noreply.github.com>
2026-04-27 18:18:37 +0700

    Time: 4 ms (48.87%) | Memory: 20.5 MB (38.24%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -3,6 +3,17 @@ class Solution:
         hashmap = {}
         for i,n in enumerate(numbers):
             diff = target - n
-            if diff in hashmap:
+            if diff in hashmap: # check n in hashmap
                 return [hashmap[diff] , i+1]
             hashmap[n] = i+1
+
+
+        # # Solution 2 pointers
+        # i, j = 0, len(numbers)-1
+        # while i < j:
+        #     if numbers[i] + numbers[j] > target:
+        #         j -= 1
+        #     elif numbers[i] + numbers[j] < target:
+        #         i += 1
+        #     else: # num[i] == num[j]
+        #         return (i+1, j+1)
