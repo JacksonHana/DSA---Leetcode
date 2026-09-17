2cc0c5235c2f8bb027c9f7f2819fab8f10da51c7
JacksonTran <107548088+JacksonHana@users.noreply.github.com>
2026-09-17 19:59:19 +0700

    Time: 30 ms (25.29%) | Memory: 21.7 MB (8.96%) - LeetSync

--- 561-array-partition/array-partition.py
+++ 561-array-partition/array-partition.py
@@ -1,8 +1,28 @@
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
-        nums.sort()
+        # nums.sort()
+        # # print(nums)
+        # sum = 0
+        # for i in range(0,len(nums),2):
+        #     sum += nums[i]
+        # return sum
+
+        # nums.sort()
         # print(nums)
-        sum = 0
-        for i in range(0,len(nums),2):
-            sum += nums[i]
-        return sum
+        # pair = []
+        # for i in range(0, len(nums), 2):
+        #     # if (i + 1) < len(nums):
+        #         pair.append((nums[i], nums[i+1]))
+        # print(pair)
+        # sum = 0
+        # for i in pair:
+        #     sum += min(i)
+
+        # return sum
+
+        # nums.sort()
+        # print(nums)
+        # slicing
+        # print(nums[::2])
+        # print(sorted(nums))
+        return (sum(sorted(nums)[::2]))
